f222dd6378407499170e2d07381b744336df621f
perl <perl@jzone.me>
2016-08-11 14:46:03 +0800

    基本完成

--- static/js/js.js
+++ static/js/js.js
@@ -1,84 +1,86 @@
 ﻿;(function ( $, window, document, undefined ) {
-  function j_isAnimation(){
-    var domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
-        pfx  = '',
-        elm = document.createElement('div');
+	function j_isAnimation(){
+    		var domPrefixes = 'Webkit Moz O ms Khtml'.split(' '),
+        		pfx  = '',
+        		elm = document.createElement('div');
 
-    if( elm.style.animationName !== undefined ) {
-      return "";
-    }else{
-        for( var i = 0,len = domPrefixes.length; i < len; i++ ) {
-          if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
-            pfx = domPrefixes[ i ];
-            return "-"+pfx+"-";
-          }
-        }
-        return "";
-    }
-  }
-  //运行一次，确认前缀，提高效率
-  var j_PRE_fix = j_isAnimation();
+    		if( elm.style.animationName !== undefined ) {
+      			return "";
+    		}else{
+        			for( var i = 0,len = domPrefixes.length; i < len; i++ ) {
+		          		if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
+		            			pfx = domPrefixes[ i ];
+		            			return "-"+pfx+"-";
+		          		}
+		        	}
+        			return "";
+    		}
+  	}
+	//运行一次，确认前缀，提高效率
+	var j_PRE_fix = j_isAnimation();
 
-  $.fn.j_animation = function(option){
-    //默认参数
-    var options = {
-      name:"",
-      duration:"1s",
-      timingFunc:"linear",
-      delay:0,
-      iteration:1,
-      fillMode:"both",
-      direction:"normal"
-    }
+	$.fn.j_animation = function(option){
+	//默认参数
+		var options = {
+			name:"",
+			duration:"1s",
+			timingFunc:"linear",
+			delay:0,
+			iteration:1,
+			fillMode:"both",
+			direction:"normal"
+		}
 
-    $.extend(options,option);
+		$.extend(options,option);
 
-    var cssObj = {};
-    cssObj[j_PRE_fix+"animation-duration"] = options.duration;
-    cssObj[j_PRE_fix+"animation-timing-function"] = options.timingFunc;
-    cssObj[j_PRE_fix+"animation-delay"] = options.delay;
-    cssObj[j_PRE_fix+"animation-iteration-count"] = options.iteration;
-    cssObj[j_PRE_fix+"animation-fill-mode"] = options.fillMode;
-    cssObj[j_PRE_fix+"animation-direction"] = options.direction;
-    cssObj[j_PRE_fix+"animation-name"] = options.name;
-    this.css(cssObj);
-  }
+		var cssObj = {};
+		cssObj[j_PRE_fix+"animation-duration"] = options.duration;
+		cssObj[j_PRE_fix+"animation-timing-function"] = options.timingFunc;
+		cssObj[j_PRE_fix+"animation-delay"] = options.delay;
+		cssObj[j_PRE_fix+"animation-iteration-count"] = options.iteration;
+		cssObj[j_PRE_fix+"animation-fill-mode"] = options.fillMode;
+		cssObj[j_PRE_fix+"animation-direction"] = options.direction;
+		cssObj[j_PRE_fix+"animation-name"] = options.name;
+		this.css(cssObj);
+	}
 
-  $.fn.j_transform = function(option){
-    var options = {
-      x:0,
-      y:0,
-      scale:1,
-      rotate:"0deg"
-    }
-    $.extend(options,option);
+	$.fn.j_transform = function(option){
+		var options = {
+			x:0,
+			y:0,
+			scale:1,
+			rotate:"0deg"
+		}
+		$.extend(options,option);
 
-    var cssObj = {};
-    cssObj[j_PRE_fix+"transform"] = "translate("+options.x+","+options.y+") scale("+options.scale+") rotate("+options.rotate+")";
+		var cssObj = {};
+		cssObj[j_PRE_fix+"transform"] = "translate("+options.x+","+options.y+") scale("+options.scale+") rotate("+options.rotate+")";
 
-    this.css(cssObj);
-  }
+		this.css(cssObj);
+	}
 
-  $.fn.j_transition = function(option){
-    //参数中关于时间的必须加上“s”
-    var options = {
-      property:"all",
-      timingFunc:"linear",
-      duration:"1s",
-      delay:"0s"
-    }
-    $.extend(options,option);
+	$.fn.j_transition = function(option){
+	//参数中关于时间的必须加上“s”
+		var options = {
+			property:"all",
+			timingFunc:"linear",
+			duration:"1s",
+			delay:"0s"
+		}
+		$.extend(options,option);
 
-    var str = options.property+" "+options.duration+" "+options.timingFunc+" "+options.delay;
+		var str = options.property+" "+options.duration+" "+options.timingFunc+" "+options.delay;
 
-    if(j_PRE_fix == ""){
-      this[0].style["transition"] = str;
-    }else{
-      var temp = j_PRE_fix.replace(/\-/g,"");
-      temp = temp.substring(0,1).toUpperCase()+temp.substring(1);
-      this[0].style[temp+"Transition"] = str;
-    }
-  }
+		this.each(function(){
+			if(j_PRE_fix == ""){
+				this.style["transition"] = str;
+			}else{
+				var temp = j_PRE_fix.replace(/\-/g,"");
+				temp = temp.substring(0,1).toUpperCase()+temp.substring(1);
+				this.style[temp+"Transition"] = str;
+			}
+		})
+	}
 
   	$.fn.removeAnimation = function(){
 		var cssObj = {};
@@ -95,13 +97,13 @@
 		this.removeTransition()
   	}
   	$.fn.removeTransition = function(){ 
-  		$(this).each(function(){
+  		this.each(function(){
   			if(j_PRE_fix == ""){
-	      			$(this)[0].style["transition"] = "";
+	      			this.style["transition"] = "";
 	    		}else{
 	      			var temp = j_PRE_fix.replace(/\-/g,"");
 	      			temp = temp.substring(0,1).toUpperCase()+temp.substring(1);
-	      			$(this)[0].style[temp+"Transition"] = "";
+	      			this.style[temp+"Transition"] = "";
 	    		}
   		})
   	}
@@ -135,6 +137,7 @@ sys.ts = function(e) {
 	sys.startY = e.touches[0].pageY;
 	sys.startX = e.touches[0].pageX;
 	sys.yInit = ahk.step * ahk.h;
+	$(".section").removeTransition()
 }
 //touch move
 sys.tm = function(e) {
@@ -234,7 +237,7 @@ ahk.rateY = ahk.h / ahk.init_h;
 
 ahk.init = function(){
 	ahk.openid = $("#openid").val();
-	$("html,body,.section,.section img.bg").css({width:ahk.w+"px",height:ahk.h+"px"});
+	$("html,body,.section,.section img.bg,.maskbox").css({width:ahk.w+"px",height:ahk.h+"px"});
 	$(".section").each(function(index){
 		var te = $(this);
 		te.attr("id","section_"+index);
@@ -242,22 +245,71 @@ ahk.init = function(){
 	
 	sys.inittouch("body");
 	ahk.loadCheck();
+
+	$(".rfsera").click(function(e){
+		e.stopPropagation();
+		$(".fsera").j_transform({y:"100%"});
+		var id = $(this).attr("data-sc");
+		setTimeout(function(){
+			$("#anmf_"+id).j_transform({y:0})
+		},100)
+	});
+	$(".maskbox").click(function(){
+		$(".fsera").j_transform({y:"100%"});
+	})
+}
+
+ahk.fsera = function(){
+	$(".fsera").j_transform({y:"100%"});
+	setTimeout(function(){
+		$(".fsera").j_transition({timingFunc:"ease",duration:"0.6s"});
+	},200)
 }
 
 
 //场景内部效果
 ahk.scene = function(){
-	$("img,.tips,.resizeform").removeAnimation();
+	$("img,.tips,.resizeform,fsera").removeAnimation();
 
 	switch(ahk.step){
 		case 0:
 			//$("#anm1_1").j_animation({name:"fadeIn",duration:"2s",timingFunc:"linear",delay:"0.2s"});
-			$("#anm1_1").j_animation({name:"fadeInDown",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".2s"});
+			$("#anm1_1").j_animation({name:"fadeInDown",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".2s"});
 			$("#anm1_2").j_animation({name:"fadeInLeft",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".5s"});
-			$("#anm1_3").j_animation({name:"fadeInRight",timingFunc:"cubic-bezier(0.04, 1.32, 0.82, 1.17)",delay:".8s"});
+			$("#anm1_3").j_animation({name:"fadeInRight",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".8s"});
 			sys.tips();
 			break;
-		
+		case 1:
+			$("#anm2_13").j_animation({name:"fadeInDown",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".1s"});
+			$("#anm2_1").j_animation({name:"zoomDown",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"0.2s"});
+			$("#anm2_2").j_animation({name:"fadeInRight",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"1.4s"});
+
+			$("#anm2_3").j_animation({name:"zoomDown",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"0.5s"});
+			$("#anm2_4").j_animation({name:"fadeInRight",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"1.7s"});
+
+			$("#anm2_5").j_animation({name:"zoomDown",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"0.8s"});
+			$("#anm2_6").j_animation({name:"fadeInRight",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"2s"});
+
+			$("#anm2_7").j_animation({name:"zoomDown",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"1.1s"});
+			$("#anm2_8").j_animation({name:"fadeInRight",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"2.3s"});
+
+			$("#anm2_12").j_animation({name:"fadeInLeft",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"1.4s"});
+			$("#anm2_11").j_animation({name:"fadeInLeft",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"1.7s"});
+			$("#anm2_10").j_animation({name:"fadeInLeft",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"2s"});
+			$("#anm2_9").j_animation({name:"fadeInLeft",duration:"0.6s",timingFunc:"cubic-bezier(0.15, 1.04, 0.84, 0.99)",delay:"2.3s"});
+			sys.tips();
+			break;
+		case 2:
+			ahk.fsera();
+			$("#anm3_0").j_animation({name:"fadeInDown",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".1s"});
+			$("#anm3_1").j_animation({name:"fadeInLeft",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".3s"});
+			$("#anm3_2").j_animation({name:"fadeInRight",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".3s"});
+			$("#anm3_3").j_animation({name:"fadeInUp",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:".9s"});
+			$("#anm3_14").j_animation({name:"fadeInUp",duration:"0.6s",timingFunc:"cubic-bezier(0.17, 0.5, 0.25, 1)",delay:"5s"});
+
+			$(".rfsera").each(function(i){
+				$("#anm3_"+(4+i)).j_animation({name:"bounceIn",duration:"1.2s",timingFunc:"cubic-bezier(0.63, 0.07, 0.41, 0.98)",delay:(1+0.3*(i+1))+"s"});
+			})
 	}
 }
 
